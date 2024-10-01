af4f41b850587fd8786ae711b8db0ede80fba90b
ManuelSpinola <mspinola10@gmail.com>
2024-10-01 17:25:23 -0600

    create_cat_esa_10m

--- R/create_cat_esa_10m.R
+++ R/create_cat_esa_10m.R
@@ -1,18 +1,35 @@
 #'
 #' @name create_cat_esa_10m
 #'
-#' @title Create a categorical land cover raster from
-#'  \href{https://esa-worldcover.org/en}{ESA WorldCover}
+#' @title Create Categorical Land Cover Raster from Copernicus
+#' Land Cover Data \href{https://esa-worldcover.org/en}{ESA WorldCover}
 #'
-#' @description Create a categorical land cover raster from
-#'  a land cover raster from copernicus 10m resolution
+#' @description
+#' This function loads a Copernicus land cover raster,
+#' reclassifies it into specified land cover categories, and
+#' optionally saves the reclassified raster to a file. The
+#' reclassification is based on predefined original classes and
+#' their corresponding new categorical classes.
 #'
-#' @param input_raster Path to your input raster file, which
-#'  should be the original Copernicus land cover raster.
-#' @param output_raster (optional): Path where the categorized
-#'  raster should be saved. If NULL, the raster is not saved to disk.
+#' @usage
+#' create_cat_esa_10m(input_raster, output_raster = NULL)
 #'
-#' @return A raster
+#' @param input_raster A character string specifying the file
+#' path to the input raster (Copernicus land cover raster) that
+#' is to be reclassified.
+#' @param output_raster A character string specifying the file
+#' path where the output categorical raster will be saved.
+#' If `NULL`, the raster will not be saved.
+#'
+#' @return A `SpatRaster` object containing the reclassified
+#' categorical land cover raster.
+#'
+#' @details
+#' The function defines a mapping of original land cover classes
+#' to new categorical classes. The reclassification is performed
+#' using the `levels` and `coltab` functions from the `terra`
+#' package. The reclassified raster will have colors associated
+#' with each class as defined in the `coltb` data frame.
 #'
 #' @references
 #'
@@ -43,6 +60,13 @@
 #'
 #' \href{https://esa-worldcover.org/en}{ESA WorldCover}
 #'
+#' @examples
+#' \dontrun{
+#' # Create categorical land cover raster
+#' categorical_raster <- create_cat_esa_10m("path_to_input_raster.tif",
+#'                                            "path_to_output_raster.tif")
+#' }
+#'
 #' @export
 #'
 
@@ -62,7 +86,7 @@ create_cat_esa_10m <- function(input_raster, output_raster = NULL) {
 
   cls <- data.frame(
     value = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100), # original classes
-    cover = c("Tree_cover", "Shrubland", "Grassland", "Cropland", "Built_up", "Bare_sparse_vegetation", "Snow_and_Ice", "Permanent_water_bodies", "Herbaceous_wetland", "Mangroves", "Moss_and_lichen") # new categorical classes
+    lulc = c("Tree_cover", "Shrubland", "Grassland", "Cropland", "Built_up", "Bare_sparse_vegetation", "Snow_and_Ice", "Permanent_water_bodies", "Herbaceous_wetland", "Mangroves", "Moss_and_lichen") # new categorical classes
   )
 
   # Reclassify the raster to categorical values
