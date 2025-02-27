83b83b24ae52c5426e101efcef378717de067ffa
ManuelSpinola <mspinola10@gmail.com>
2025-02-27 11:27:48 -0600

    limit get_records

--- R/get_records.R
+++ R/get_records.R
@@ -104,7 +104,7 @@ get_records <- function(species_name,
   # Extracting the genus and species from the name column
   if ("name" %in% names(combined_df)) {
     combined_df <- combined_df |>
-      mutate(species = sapply(strsplit(as.character(name), " "), function(x) paste(x[1:2], collapse = "_")))
+      dplyr::mutate(species = sapply(strsplit(as.character(name), " "), function(x) paste(x[1:2], collapse = "_")))
   }
 
   # Convert the combined data frame to an sf object
