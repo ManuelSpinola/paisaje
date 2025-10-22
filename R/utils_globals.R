be8181512237e0bba37d39f1775b43228c119238
ManuelSpinola <mspinola10@gmail.com>
2025-10-22 09:04:32 -0600

    utils_global.R

--- R/utils_globals.R
+++ R/utils_globals.R
@@ -6,9 +6,14 @@ magrittr::`%>%`
 # Declare global variables used inside dplyr pipelines
 # This avoids "no visible binding" notes in R CMD check
 utils::globalVariables(c(
+  ".data",
   "h3_address",
   "plot_id",
   "geometry",
   "metric",
-  "value"
+  "value",
+  "n",
+  "sp_name",
+  "sp_col",
+  "hex_id"
 ))
